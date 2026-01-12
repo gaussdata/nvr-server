3a7482f6d35553bcfa408523c2f850d88e0eeee0
gausszhou <gausszhou@qq.com>
2026-01-12 23:59:14 +0800

    refactor: 更新依赖并添加中间件支持
    - 将express从5.x降级到4.x以保持稳定性
    - 移除未使用的onvif依赖
    - 添加cors和log中间件支持
    - 添加vscode配置以启用eslint自动修复

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import middlewareCors from './middlewares/cors.js'
+import middlewareLog from './middlewares/log.js'
 import logger from './utils/logger.js'
 
 const app = express()
@@ -8,6 +10,11 @@ app.set('trust proxy', 1)
 app.use(express.text())
 // json 解析
 app.use(express.json({ limit: '1024kb' }))
+
+// cors 跨域
+app.use(middlewareCors) // or use nginx
+
+app.use(middlewareLog)
 
 // 当客户端以get方式访问/路由时
 app.get('/', (req: express.Request, res: express.Response) => {
